fdee4272f17b3a89bc68b9e7a5ce13f12493ea0d
murilinhops <murilinhorps@gmail.com>
2021-05-05 18:17:52 -0300

    Add appModule to render appWidget

---
+++ lib/src/application/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:joguinho_detetive/src/application/app_widget.dart';
+
+class AppModule extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppWidget(title: 'Detetive');
+  }
+}
